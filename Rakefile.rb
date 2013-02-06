3bb491187d26b88bf8354ba6a2a245f4979ca8fd
James <e@mailjames.com>
2013-02-06 23:10:22 +0000

    rake deployment

--- Rakefile.rb
+++ Rakefile.rb
@@ -5,6 +5,19 @@ task :npm do
     sh "npm install"
 end
 
+task :stopService do
+    sh "sudo service <<ServiceName>> stop"
+end
+
+task :startService do
+    sh "sudo service <<ServiceName>>start"
+end
+
+task :updateCode do
+    sh "git checkout master"
+    sh "git pull origin master"
+end
+
 task :runProgram do
   sh "node index.js"
 end

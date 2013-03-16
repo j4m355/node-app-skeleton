c42471694ebafbb157195cb1dc80433fb319fcb2
James <e@mailjames.com>
2013-03-16 21:36:26 +0000

    _settings.json support

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,10 +1,18 @@
-task :install => [:npm]
+task :install => [:npm, :createLocalSettings]
 task :default => [:runProgram]
 task :build => [:stopService, :npm, :startService]
 
 
 task :npm do
     sh "npm install"
+end
+
+task :createLocalSettings do
+	Dir.chdir "app" do
+		Dir.chdir "config" do
+			sh "cp settings.json _settings.json"
+		end
+	end
 end
 
 task :stopService do
